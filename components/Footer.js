1b1af4b573a752e83728a19037cb89168008b353
Marcos-Mutuma7 <maccosmutuma1981@gmail.com>
2021-05-31 20:03:32 +0300

    initial

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@ import {faTwitter ,faFacebookF,faGithub ,faWhatsapp}  from "@fortawesome/free-br
 import {faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import {faMapMarker} from "@fortawesome/free-solid-svg-icons"
 import {faPhone , faCopyright}  from "@fortawesome/free-solid-svg-icons"
+import Link from 'next/link'
 
 
 const Footer = () => {
@@ -18,15 +19,19 @@ const Footer = () => {
            <FontAwesomeIcon className={styles.ph} icon={faPhone}></FontAwesomeIcon>
           <FontAwesomeIcon className={styles.gm} icon={faEnvelope}></FontAwesomeIcon>
           <FontAwesomeIcon className={styles.mp} icon={faMapMarker}><h1>the</h1></FontAwesomeIcon>
+
+          <Link href='https://wa.me/254757929917'>
+          <FontAwesomeIcon className={styles.wp} icon={faWhatsapp}></FontAwesomeIcon>
+          </Link>
           <h3 className={styles.footer__h3}>Nairobi ,Kenya</h3>
           <FontAwesomeIcon className={styles.cp} icon={faCopyright}></FontAwesomeIcon>
           <h4 className={styles.ma}>2021 Macc Africa</h4>
+          
+        
      </div>
      
-       <div className={styles.whatts__app}>
-          <FontAwesomeIcon className={styles.wp} icon={faWhatsapp}></FontAwesomeIcon>
-          
-        </div>
+       
+         
 
          </footer>
             
