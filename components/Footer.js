2bbcad4e6f89dbd9b2eaf207faaaa27c0a57caf6
Marcos-Mutuma7 <maccosmutuma1981@gmail.com>
2021-06-03 09:15:24 +0300

    initial

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Styles from '../styles/components.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faTwitter ,faFacebookF,faGithub ,faWhatsapp}  from "@fortawesome/free-brands-svg-icons"
 import {faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import {faMapMarker} from "@fortawesome/free-solid-svg-icons"
+import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons"
 import {faPhone , faCopyright}  from "@fortawesome/free-solid-svg-icons"
 import Link from 'next/link'
 
@@ -18,7 +18,7 @@ const Footer = () => {
            <FontAwesomeIcon className={styles.gt} icon={faGithub}></FontAwesomeIcon>
            <FontAwesomeIcon className={styles.ph} icon={faPhone}></FontAwesomeIcon>
           <FontAwesomeIcon className={styles.gm} icon={faEnvelope}></FontAwesomeIcon>
-          <FontAwesomeIcon className={styles.mp} icon={faMapMarker}><h1>the</h1></FontAwesomeIcon>
+          <FontAwesomeIcon className={styles.mp} icon={faMapMarkerAlt}><h1>the</h1></FontAwesomeIcon>
 
           
           <h3 className={styles.footer__h3}>Nairobi Kenya</h3>
